e662ef281a44f5f477d65e262ab41ca54a468855
saifullah rahman <saiflll284@gmail.com>
2022-05-15 13:15:49 +0530

    fixed project image responsive issue

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -30,22 +30,24 @@ function Projects() {
 				<h1 className="text-theme-gray-light  min-w-fit ml-[10px] text-[26px] sm:text-[32px]  flex justify-center items-center font-calibreSemiBold fontFix">Some things I've built</h1>
 			</div>
 			<div className="flex flex-col  justify-center items-center min-h-full text-[20px] font-calibreRegular text-theme-gray-deep font-medium ">
-				<div className="mb-[100px] flex flex-row w-full h-full min-h-full">
-					<div className="max-w-[60%] image-top ">
-						<div className="image-cover ">
-							<img src={jobbedInHome} alt="JobbedIn home page" className="" />
+				<div className="flex flex-row justify-end  w-full relative lg:mt-[50px] ">
+					<div className="absolute left-0 top-1/2 translate-y-[-50%]">
+						<div className="max-w-[60%] max-h-[100%] image-top  ">
+							<div className="image-cover ">
+								<img src={jobbedInHome} alt="JobbedIn home page" className="" />
+							</div>
 						</div>
 					</div>
-					<div className="max-w-[40%] w-[40%] flex flex-col items-end h-full ">
-						<div className="text-theme-green my-2 mt-[40px] font-sfMonoRegular text-[13px]">Featured Project</div>
+					<div className="max-w-[60%] flex flex-col items-end h-full ">
+						<div className="text-theme-green my-2 font-sfMonoRegular text-[13px]">Featured Project</div>
 						<h1 className="font-calibreSemiBold text-[28px] text-theme-gray-light mb-5">JobbedIn</h1>
 						<div className="relative w-full">
-							<div className="p-6 absolute top-0 right-0 w-[120%] bg-theme-light-navy rounded text-theme-light-slate z-10">
+							<div className="p-6   bg-theme-light-navy rounded text-theme-light-slate z-10">
 								<p>A Full Stack Job Portal website for Employers and candidates. Separate accounts for both employers and candidates. </p>
 							</div>
 						</div>
 						<div className=" relative w-full z-10 my-10">
-							<div className="flex flex-row flex-wrap w-[120%]  absolute bottom-0 right-0   rounded text-theme-light-slate z-11">
+							<div className="flex flex-row flex-wrap w-[120%]    rounded text-theme-light-slate z-11">
 								<div className="ml-[20px] mb-[5px]">React</div>
 								<div className="ml-[20px] mb-[5px]">Express</div>
 								<div className="ml-[20px] mb-[5px]">MongoDB</div>
