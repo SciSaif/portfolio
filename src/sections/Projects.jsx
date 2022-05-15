3f54cde5845aaa2e2358877f8b4fd8e02a6c88fd
saifullah rahman <saiflll284@gmail.com>
2022-05-15 11:39:57 +0530

    test

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -30,9 +30,9 @@ function Projects() {
 				<h1 className="text-theme-gray-light  min-w-fit ml-[10px] text-[26px] sm:text-[32px]  flex justify-center items-center font-calibreSemiBold fontFix">Some things I've built</h1>
 			</div>
 			<div className="flex flex-col  justify-center items-center min-h-full text-[20px] font-calibreRegular text-theme-gray-deep font-medium ">
-				<div className="mb-[100px] flex flex-row w-full h-full ">
+				<div className="mb-[100px] flex flex-row w-full h-full min-h-full">
 					<div className="max-w-[60%] image-top ">
-						<div className="image-cover bg-black">
+						<div className="image-cover ">
 							<img src={jobbedInHome} alt="JobbedIn home page" className="" />
 						</div>
 					</div>
@@ -44,13 +44,13 @@ function Projects() {
 								<p>A Full Stack Job Portal website for Employers and candidates. Separate accounts for both employers and candidates. </p>
 							</div>
 						</div>
-						<div className="relative w-full">
-							<div className="p-6 flex flex-row  absolute bottom-0 right-0 w-fit  rounded text-theme-light-slate">
-								<div>React</div>
-								<div>Express</div>
-								<div>MongoDB</div>
-								<div>Redux</div>
-								<div>Tailwind CSS</div>
+						<div className=" relative w-full z-10 my-10">
+							<div className="flex flex-row flex-wrap w-[120%]  absolute bottom-0 right-0   rounded text-theme-light-slate z-11">
+								<div className="ml-[20px] mb-[5px]">React</div>
+								<div className="ml-[20px] mb-[5px]">Express</div>
+								<div className="ml-[20px] mb-[5px]">MongoDB</div>
+								<div className="ml-[20px] mb-[5px]">Redux</div>
+								<div className="ml-[20px] mb-[5px]">Tailwind CSS</div>
 							</div>
 						</div>
 					</div>
