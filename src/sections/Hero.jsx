6024d9096720f53df2f54f79fec9e81cf3604522
saifullah rahman <saiflll284@gmail.com>
2022-06-09 00:58:08 +0530

    edit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,7 +25,8 @@ function Hero() {
 			<h2 className="name-heading font-calibreRegular text-[40px] font-bold text-theme-gray-light pt-0">Saifullah Rahman.</h2>
 			<h3 className="sub-heading font-calibreRegular text-[40px] font-bold text-theme-gray-deep pt-0 ">I build cool stuff for the web.</h3>
 			<p className="bio max-w-[540px] mt-[20px] text-[20px] text-theme-gray-deep font-calibreRegular">
-				I'm a full stack web developer with a passion for building beautiful, responsive websites.
+				I'm a full stack web developer with a passion for building beautiful, responsive websites. I am currently focusing on the MERN stack and I am looking to expand my knowledge and
+				skillset.
 			</p>
 			<div className="w-fit mt-9 border border-theme-green hover:bg-theme-green/10 cursor-pointer rounded flex  items-center text-theme-green">
 				<a target="_blank" rel="noreferrer" href="https://github.com/SciSaif" className=" px-[50px] py-[18px]">
