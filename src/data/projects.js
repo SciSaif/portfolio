ae8083beb6c9f9e98136ba68f74c26a0dc900608
saifullah rahman <saiflll284@gmail.com>
2023-08-13 19:09:15 +0530

    resume change

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,66 +1,44 @@
 export const data = {
-    projects: [
-        {
-            title: "JobbedIn",
-            description:
-                "A Full Stack Job Portal website for Employers and candidates. Separate accounts for both employers and candidates. ",
-            technologies: [
-                "React",
-                "Express",
-                "Mongo DB",
-                "Redux",
-                "Tailwind CSS",
-            ],
-            github: "https://github.com/SciSaif/JobbedIn",
-            website: "https://jobbedin.herokuapp.com/",
-            img: require("@assets/jobbedInHome.png"),
-        },
-        // {
-        // 	title: "To-do List App",
-        // 	description: "This is a simple but functional To Do App, with user login and authentication ",
-        // 	technologies: ["React", "Firebase", "Tailwind CSS", "Daisy UI", "Framer Motion"],
-        // 	github: "https://github.com/SciSaif/To-Do-List",
-        // 	website: "https://to-do-list-scisaif.vercel.app/",
-        // 	img: require("@assets/toDoListHome.png"),
-        // },
-        {
-            title: "SciMeet",
-            description:
-                "A video conferencing web app that allows users to create and join rooms. Users can also chat with each other",
-            technologies: [
-                "React",
-                "Express",
-                "Typescript",
-                "Socket.io",
-                "WebRTC",
-                "MongoDB",
-            ],
-            github: "https://github.com/SciSaif/sciMeet-client",
-            website: "https://scimeet.vercel.app/",
-            img: require("@assets/scimeet.png"),
-        },
-        {
-            title: "AIQatar.qa",
-            description:
-                "Led end-to-end Next.js site creation, optimizing UX with multistep appointments and event registration. Streamlined admin control for events, appointments, and partners. Integrated secure Sadad payments and efficient SendGrid communication. Engineered dynamic events page with enhanced user and admin functionalities. Amplified operational efficiency and user satisfaction.",
-            technologies: [
-                "Next.js",
-                "Express",
-                "Typescript",
-                "Sendgrid",
-                "Sadad Payment Gateway",
-            ],
-            github: "https://github.com/SciSaif",
-            website: "https://aiqatar.qa/",
-            img: require("@assets/aiqatar.png"),
-        },
-        // {
-        // 	title: "My Portfolio website",
-        // 	description: "This is my portfolio website that I created using react and tailwind. The design for this website was inspired by Brittany Chiang's portfolio website. ",
-        // 	technologies: ["React", "Tailwind CSS", "Framer Motion", "react-scroll"],
-        // 	github: "https://github.com/SciSaif/portfolio",
-        // 	website: "https://portfolio-scisaif.vercel.app/",
-        // 	img: require("@assets/portfolioHome.png"),
-        // },
-    ],
+	projects: [
+		{
+			title: "JobbedIn",
+			description: "A Full Stack Job Portal website for Employers and candidates. Separate accounts for both employers and candidates. ",
+			technologies: ["React", "Express", "Mongo DB", "Redux", "Tailwind CSS"],
+			github: "https://github.com/SciSaif/JobbedIn",
+			website: "https://jobbedin.herokuapp.com/",
+			img: require("@assets/jobbedInHome.png"),
+		},
+		// {
+		// 	title: "To-do List App",
+		// 	description: "This is a simple but functional To Do App, with user login and authentication ",
+		// 	technologies: ["React", "Firebase", "Tailwind CSS", "Daisy UI", "Framer Motion"],
+		// 	github: "https://github.com/SciSaif/To-Do-List",
+		// 	website: "https://to-do-list-scisaif.vercel.app/",
+		// 	img: require("@assets/toDoListHome.png"),
+		// },
+		{
+			title: "SciMeet",
+			description: "A video conferencing web app that allows users to create and join rooms. Users can also chat with each other",
+			technologies: ["React", "Express", "Typescript", "Socket.io", "WebRTC", "MongoDB"],
+			github: "https://github.com/SciSaif/sciMeet-client",
+			website: "https://scimeet.vercel.app/",
+			img: require("@assets/scimeet.png"),
+		},
+		{
+			title: "Aiqatar.qa",
+			description: "Led end-to-end Next.js site creation, optimizing UX with multistep appointments and event registration. Streamlined admin control for events, appointments, and partners. Integrated secure Sadad payments and efficient SendGrid communication. Engineered dynamic events page with enhanced user and admin functionalities. Amplified operational efficiency and user satisfaction.",
+			technologies: ["React", "Express", "Typescript", "Socket.io", "WebRTC", "MongoDB"],
+			github: "https://github.com/SciSaif/sciMeet-client",
+			website: "https://scimeet.vercel.app/",
+			img: require("@assets/scimeet.png"),
+		}
+		// {
+		// 	title: "My Portfolio website",
+		// 	description: "This is my portfolio website that I created using react and tailwind. The design for this website was inspired by Brittany Chiang's portfolio website. ",
+		// 	technologies: ["React", "Tailwind CSS", "Framer Motion", "react-scroll"],
+		// 	github: "https://github.com/SciSaif/portfolio",
+		// 	website: "https://portfolio-scisaif.vercel.app/",
+		// 	img: require("@assets/portfolioHome.png"),
+		// },
+	],
 };
